2333d0a3319b1c39e257361fe900ce784b34a9df
Ahmet Hüseyin Dok <ahmet.dok@qbitra.com>
2021-04-11 10:14:09 +0300

    some code improvements

--- query.js
+++ query.js
@@ -1,8 +1,10 @@
 const puppeeter = require("puppeteer");
+
 /**
  * @type {puppeeter.Browser}
  */
 let browser;
+
 async function getProperty(element, selector, content = "textContent") {
   const innerElem = await element.$(selector);
   let innerHandler = await innerElem.getProperty(content);
@@ -20,6 +22,40 @@ async function closeBrowser() {
   browser.close();
 }
 
+async function extractArticleData(article) {
+  const nameSelector = "span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0";
+  const idSelector = "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > span";
+  const timeSelector = "time";
+  const textSelector = "div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu";
+
+  let dataMapping = {
+    name: nameSelector,
+    id: idSelector,
+    text: textSelector,
+  };
+
+  let data = {};
+
+  await Promise.all(
+    Object.entries(dataMapping).map(async ([key, value]) => {
+      try {
+        data[key] = await getProperty(article, value);
+      } catch {
+        console.log("key not found: " + key);
+      }
+    })
+  );
+
+  const timeElement = await article.$(timeSelector);
+  if (timeElement) {
+    data["time"] = await timeElement.evaluate((element) => {
+      return element.getAttribute("datetime");
+    });
+  }
+
+  return data;
+}
+
 async function query(query, scroll = 20) {
   console.log(`searching for query ${query} with scroll count ${scroll}`);
   const page = await browser.newPage();
@@ -27,49 +63,16 @@ async function query(query, scroll = 20) {
   await page.goto("https://twitter.com/hashtag/" + escape(query) + "?f=live", {
     waitUntil: "networkidle2",
   });
-  // function waitInSeconds(seconds) {
-  //   new Promise((res) => setTimeout(res, seconds * 1000));
-  // }
+
   let total = [];
 
   for (let i = 0; i < scroll; i++) {
     let articles = await page.$$("article");
 
-    let data = await Promise.all(
-      articles.map(async (article) => {
-        const nameSelector = "span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0";
-        const idSelector = "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > span";
-        const timeSelector = "time";
-        let dataMapping = {
-          name: nameSelector,
-          id: idSelector,
-          text: "div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu",
-        };
-        let data = {};
-
-        await Promise.all(
-          Object.entries(dataMapping).map(async ([key, value]) => {
-            console.log("searching for: " + key);
-            try {
-              data[key] = await getProperty(article, value);
-            } catch {
-              console.log("key not found: " + key);
-            }
-          })
-        );
-
-        const timeElement = await article.$(timeSelector);
-        if (timeElement) {
-          data["time"] = await timeElement.evaluate((element) => {
-            return element.getAttribute("datetime");
-          });
-        }
-
-        return data;
-      })
-    );
+    let data = await Promise.all(articles.map(extractArticleData));
 
     for (const tweet of data) {
+      //check if the tweet already exists
       if (total.find((item) => item.text == tweet.text)) {
         continue;
       }
