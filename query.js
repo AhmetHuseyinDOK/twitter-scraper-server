259d7c92fc3394293fb8ed45b52427cbf237ee93
Ahmet Hüseyin Dok <ahmet.dok@qbitra.com>
2021-05-15 13:46:33 +0300

    get data directly from network request

--- query.js
+++ query.js
@@ -1,15 +1,8 @@
 const puppeeter = require("puppeteer");
-
 /**
  * @type {puppeeter.Browser}
  */
 let browser;
-
-async function getProperty(element, selector, content = "textContent") {
-  const innerElem = await element.$(selector);
-  let innerHandler = await innerElem.getProperty(content);
-  return await innerHandler.jsonValue();
-}
 
 async function openBrowser() {
   browser = await puppeeter.launch({
@@ -22,71 +15,43 @@ async function closeBrowser() {
   browser.close();
 }
 
-async function extractArticleData(article) {
-  const nameSelector = "span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0";
-  const idSelector = "div.css-1dbjc4n.r-18u37iz.r-1wbh5a2.r-13hce6t > div > span";
-  const timeSelector = "time";
-  const textSelector = "div.css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-kzbkwu";
-
-  let dataMapping = {
-    name: nameSelector,
-    id: idSelector,
-    text: textSelector,
-  };
-
-  let data = {};
-
-  await Promise.all(
-    Object.entries(dataMapping).map(async ([key, value]) => {
-      try {
-        data[key] = await getProperty(article, value);
-      } catch {
-        console.log("key not found: " + key);
-      }
-    })
-  );
-
-  const timeElement = await article.$(timeSelector);
-  if (timeElement) {
-    data["time"] = await timeElement.evaluate((element) => {
-      return element.getAttribute("datetime");
-    });
-  }
-
-  return data;
-}
-
 async function query(query, scroll = 20) {
   console.log(`searching for query ${query} with scroll count ${scroll}`);
   const page = await browser.newPage();
   page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36");
-  await page.goto("https://twitter.com/hashtag/" + escape(query) + "?f=live", {
-    waitUntil: "networkidle2",
-  });
+  await page.setRequestInterception(true);
+  page.goto("https://twitter.com/hashtag/" + escape(query) + "?f=live").catch(() => {});
+  let data = await new Promise((res, rej) => {
+    page.on("request", (req) => req.continue());
+    page.on("requestfinished", async (request) => {
+      const response = await request.response();
 
-  let total = [];
-
-  for (let i = 0; i < scroll; i++) {
-    let articles = await page.$$("article");
-
-    let data = await Promise.all(articles.map(extractArticleData));
-
-    for (const tweet of data) {
-      //check if the tweet already exists
-      if (total.find((item) => item.text == tweet.text)) {
-        continue;
+      const responseHeaders = response.headers();
+      let responseBody;
+      if (request.redirectChain().length === 0) {
+        // body can only be access for non-redirect responses
+        try {
+          responseBody = await response.buffer();
+        } catch {}
       }
 
-      total.push(tweet);
-    }
+      const information = {
+        url: request.url(),
+        requestHeaders: request.headers(),
+        requestPostData: request.postData(),
+        responseHeaders: responseHeaders,
+        responseSize: responseHeaders["content-length"],
+        responseBody,
+      };
 
-    await page.evaluate(async () => {
-      window.scrollBy(0, 500);
+      if (information.url.includes("twitter.com/i/api/2/search/adaptive.json")) {
+        res(JSON.parse(information.responseBody.toString()));
+      }
     });
-  }
-
+  });
   page.close();
-  return total;
+  let { tweets, users } = data.globalObjects;
+  return { tweets, users };
 }
 
 module.exports = {
