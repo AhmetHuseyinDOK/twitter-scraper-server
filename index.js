6099c2e161cfa376efe218e442f891003cbd3ff1
Ahmet Hüseyin Dok <ahmet.dok@qbitra.com>
2021-05-16 18:53:09 +0300

    added query-short

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get("/query", async (req, res) => {
   let data = await query(req.query.query, req.query.count);
   res.send(data);
 });
+
+app.get("/query-short", async (req, res) => {
+  let { tweets, users } = await query(req.query.query, req.query.count);
+
+  let tweetList = Object.values(tweets).map((tweet) => {
+    return {
+      id: tweet.id_str,
+      text: tweet.full_text,
+      time: tweet.created_at,
+      name: users[tweet.user_id_str].name,
+    };
+  });
+
+  res.send(tweetList);
+});
+
 console.log("starting server...");
 console.log("opening browser");
 openBrowser().then(() => {
